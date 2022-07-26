b93cdae6345b329cd5f100cdbd33ebca9283ac86
efeldel <man-9k@yandex.ru>
2022-07-26 21:02:26 +0300

    add input validation

--- InteractRunner.java
+++ InteractRunner.java
@@ -6,26 +6,30 @@ public class InteractRunner {
             Calculator calc = new Calculator();
             while (true) {
                 calc.cleanResult();
-                System.out.println("Введите первое число:");
-                int first = sc.nextInt();
-                System.out.println("Введите второе число:");
-                int second = sc.nextInt();
-                System.out.println("Выберите действие:");
-                System.out.println("1. Сложить");
-                System.out.println("2. Вычесть");
-                System.out.println("3. Умножить");
-                System.out.println("4. Разделить");
-                System.out.println("0. Выход");
-                switch (sc.nextInt()) {
-                    case 0 -> {
-                        return;
+                try {
+                    System.out.println("Введите первое число:");
+                    int first = Integer.parseInt(sc.next());
+                    System.out.println("Введите второе число:");
+                    int second = Integer.parseInt(sc.next());
+                    System.out.println("Выберите действие:");
+                    System.out.println("1. Сложить");
+                    System.out.println("2. Вычесть");
+                    System.out.println("3. Умножить");
+                    System.out.println("4. Разделить");
+                    System.out.println("0. Выход");
+                    switch (Integer.parseInt(sc.next())) {
+                        case 0 -> {
+                            return;
+                        }
+                        case 1 -> calc.add(first, second);
+                        case 2 -> calc.sub(first, second);
+                        case 3 -> calc.mul(first, second);
+                        case 4 -> calc.div(first, second);
                     }
-                    case 1 -> calc.add(first, second);
-                    case 2 -> calc.sub(first, second);
-                    case 3 -> calc.mul(first, second);
-                    case 4 -> calc.div(first, second);
+                    System.out.println("Результат: " + calc.getResult());
+                } catch (NumberFormatException e) {
+                    System.out.println("Недопустимый ввод, разрешены только числа.");
                 }
-                System.out.println("Результат: " + calc.getResult());
             }
         }
     }
